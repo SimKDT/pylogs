8d6bd91596cbdbea8ce9ddb0e2dddbe51a25875a
Simon CADET <simon.cadet@univ-poitiers.fr>
2026-07-13 11:14:11 +0200

    remove old echo and color files

--- src/color.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated module for color handling. Use pylogs.color instead."""
-
-from warnings import warn
-from pylogs import color
-
-warn("The 'color' module is deprecated. Use 'pylogs.color' instead.", DeprecationWarning, stacklevel=2)
--- src/echo.py
+++
@@ -1,6 +0,0 @@
-"""Deprecated module for echo handling. Use pylogs.echo instead."""
-
-from warnings import warn
-from pylogs import echo
-
-warn("The 'echo' module is deprecated. Use 'pylogs.echo' instead.", DeprecationWarning, stacklevel=2)
